7370cf732743308fe56252475520a12199994172
neo <neoqing@gmail.com>
2021-10-07 22:27:07 +0800

    springboot demo

---
+++ 第一部分-Spring Boot/第1章-热身运动/springbootdemo/src/main/java/com/broadview/springbootdemo/DemoController.java
@@ -0,0 +1,13 @@
+package com.broadview.springbootdemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @RequestMapping(value = "/bootdemo", method = RequestMethod.GET, produces = "application/json")
+    public String test(){
+        return "helloWorld from spring boot controller";
+    }
+}
